024af8127427bee68b65dc3582cb21e3bdeb4521
Saad Sayyed <saadsyed950@gmail.com>
2025-09-27 00:25:30 +0530

    Testing API

--- mern-jwt/server/src/server.js
+++ mern-jwt/server/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectToDB = require("./config/db");
+const gameRoutes = require("./api/routes/game.services");
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 connectToDB();
+
+app.use("/api/game", gameRoutes);
 
 app.get("/", (_req, res) => {
   res.json({ server: true });
