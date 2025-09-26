ec0adc62d97396778a3cb52a1238b62b94e77906
Saad Sayyed <saadsyed950@gmail.com>
2025-09-26 23:46:08 +0530

    Refreshing MERN RestAPI

---
+++ mern-jwt/server/src/index.js
@@ -0,0 +1,5 @@
+import { app } from "./server";
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server running on PORT:${port}`));
---
+++ mern-jwt/server/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_req, res) => {
+  res.json({ server: true });
+});
+
+module.exports = {
+  app,
+};
