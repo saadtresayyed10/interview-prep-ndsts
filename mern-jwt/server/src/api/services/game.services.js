a883d56fd342b588a9e721333a527c388b0033b3
Saad Sayyed <saadsyed950@gmail.com>
2025-09-27 00:16:01 +0530

    Game create and get all services

---
+++ mern-jwt/server/src/api/services/game.services.js
@@ -0,0 +1,18 @@
+const Game = require("../../model/Game");
+
+const createGame = async (title, genre, lore, ratings) => {
+  const exitingGame = await Game.findOne(title);
+
+  if (exitingGame) throw new Error("Game already exists");
+
+  return await Game.create({ title, genre, lore, ratings });
+};
+
+const getAllGames = async () => {
+  return await Game.find();
+};
+
+module.exports = {
+  createGame,
+  getAllGames,
+};
