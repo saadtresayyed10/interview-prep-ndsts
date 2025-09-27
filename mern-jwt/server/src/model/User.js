28d629edd2132c798bc133224d942bac6a9e1ea0
Saad Sayyed <saadsyed950@gmail.com>
2025-09-27 11:07:34 +0530

    User model

---
+++ mern-jwt/server/src/model/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 8,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
